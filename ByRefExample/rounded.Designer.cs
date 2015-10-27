f11c0e45953d879947424001d0d21c737e104d2e
PerryMartin <perry.martin@ocsbstudent.ca>
2015-10-27 13:11:18 -0400

    v1.0

---
+++ ByRefExample/rounded.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ByRefExample
+{
+    partial class frmRounded
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumRound = new System.Windows.Forms.TextBox();
+            this.btnCalculator = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNumRound
+            // 
+            this.txtNumRound.Location = new System.Drawing.Point(49, 46);
+            this.txtNumRound.Name = "txtNumRound";
+            this.txtNumRound.Size = new System.Drawing.Size(109, 20);
+            this.txtNumRound.TabIndex = 0;
+            // 
+            // btnCalculator
+            // 
+            this.btnCalculator.Location = new System.Drawing.Point(49, 83);
+            this.btnCalculator.Name = "btnCalculator";
+            this.btnCalculator.Size = new System.Drawing.Size(109, 23);
+            this.btnCalculator.TabIndex = 1;
+            this.btnCalculator.Text = "Calculate";
+            this.btnCalculator.UseVisualStyleBackColor = true;
+            this.btnCalculator.Click += new System.EventHandler(this.btnCalculator_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(83, 130);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 13);
+            this.lblAnswer.TabIndex = 2;
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Location = new System.Drawing.Point(46, 20);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(112, 13);
+            this.lblQuestion.TabIndex = 3;
+            this.lblQuestion.Text = "Enter number to round";
+            // 
+            // frmRounded
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(193, 184);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnCalculator);
+            this.Controls.Add(this.txtNumRound);
+            this.Name = "frmRounded";
+            this.Text = "ByRef Example";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumRound;
+        private System.Windows.Forms.Button btnCalculator;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Label lblQuestion;
+    }
+}
+
---
+++ ByRefExample/rounded.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Created by: Perry Martin
+ * Created on: 27-10-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit#4-02
+ * This program displays rounded number
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ByRefExample
+{
+    public partial class frmRounded : Form
+    {
+        private void RoundNumber(ref Label answer, double number)
+        {
+            answer.Text = Convert.ToString(Math.Round(number, 1));
+        }
+    
+        public frmRounded()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculator_Click(object sender, EventArgs e)
+        {
+            Double numberInput;
+
+            numberInput = Convert.ToDouble(this.txtNumRound.Text);
+
+            RoundNumber(ref this.lblAnswer, numberInput);
+        }
+    }
+}
